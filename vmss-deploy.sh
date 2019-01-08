c35ab77e44b6907ce2cc21341d3c9e3bbc4656d6
Tam Huynh <tamhuynh@Tams-MacBook.local>
2019-01-08 09:48:50 -0800

    Updating vmss script

--- vmss-deploy.sh
+++ vmss-deploy.sh
@@ -13,7 +13,7 @@ cp -i cockroach-v2.1.3.linux-amd64/cockroach /usr/local/bin  >> $LOG_PATH 2>&1
 mkdir /var/lib/cockroach >> $LOG_PATH 2>&1
 useradd cockroach >> $LOG_PATH 2>&1
 chown cockroach /var/lib/cockroach >> $LOG_PATH 2>&1
-wget -qO- https://raw.githubusercontent.com/cockroachdb/docs/master/_includes/v2.1/prod-deployment/insecurecockroachdb.service >> $LOG_PATH 2>&1
+wget -qO- /etc/systemd/system/insecurecockroachdb.service https://raw.githubusercontent.com/cockroachdb/docs/master/_includes/v2.1/prod-deployment/insecurecockroachdb.service >> $LOG_PATH 2>&1
 
 echo done  >> $LOG_PATH 2>&1
 # Exit script with 0 code to tell Azure that the deployment is done
