b6414427cc00db119bdae1d69b1597b5bf88cfc4
Tam Huynh <tamhuynh@Tams-MacBook.local>
2019-01-11 00:30:53 -0800

    add msi reader role for vm and vmss

--- jbox-deploy.sh
+++ jbox-deploy.sh
@@ -8,6 +8,18 @@ echo $@ >> $LOG_PATH 2>&1
 date > $LOG_PATH 2>&1
 whoami >> $LOG_PATH 2>&1
 
+# install Azure CLI
+sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y > $LOG_PATH 2>&1
+AZ_REPO=$(lsb_release -cs) > $LOG_PATH 2>&1
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list > $LOG_PATH 2>&1
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+     --keyserver packages.microsoft.com \
+     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF > $LOG_PATH 2>&1
+sudo apt-get update > $LOG_PATH 2>&1
+sudo apt-get install azure-cli > $LOG_PATH 2>&1
+
+# install coackroach db
 wget -qO- https://binaries.cockroachdb.com/cockroach-v2.1.3.linux-amd64.tgz | tar  xvz  >> $LOG_PATH 2>&1
 cp -i cockroach-v2.1.3.linux-amd64/cockroach /usr/local/bin  >> $LOG_PATH 2>&1
 
--- vmss-deploy.sh
+++ vmss-deploy.sh
@@ -11,6 +11,17 @@ whoami >> $LOG_PATH 2>&1
 # Store parameters passed to this script
 NUM_OF_DATA_DISKS=${1}
 
+# install Azure CLI
+sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y > $LOG_PATH 2>&1
+AZ_REPO=$(lsb_release -cs) > $LOG_PATH 2>&1
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list > $LOG_PATH 2>&1
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+     --keyserver packages.microsoft.com \
+     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF > $LOG_PATH 2>&1
+sudo apt-get update > $LOG_PATH 2>&1
+sudo apt-get install azure-cli > $LOG_PATH 2>&1
+
 # Create the /cockroach-data data disk as a RAID 0
 COCKROACHDB_PATH=/cockroach-data
 
@@ -32,6 +43,7 @@ fi
 
 mount $COCKROACHDB_PATH
 
+# install coackroach db
 wget -qO- https://binaries.cockroachdb.com/cockroach-v2.1.3.linux-amd64.tgz | tar  xvz >> $LOG_PATH 2>&1
 cp -i cockroach-v2.1.3.linux-amd64/cockroach /usr/local/bin  >> $LOG_PATH 2>&1
 mkdir /var/lib/cockroach >> $LOG_PATH 2>&1
@@ -39,6 +51,7 @@ useradd cockroach >> $LOG_PATH 2>&1
 chown cockroach /var/lib/cockroach >> $LOG_PATH 2>&1
 chown cockroach $COCKROACHDB_PATH >> $LOG_PATH 2>&1
 wget -qO- https://raw.githubusercontent.com/cockroachdb/docs/master/_includes/v2.1/prod-deployment/insecurecockroachdb.service > /etc/systemd/system/insecurecockroachdb.service >> $LOG_PATH 2>&1
+
 
 echo done  >> $LOG_PATH 2>&1
 # Exit script with 0 code to tell Azure that the deployment is done
