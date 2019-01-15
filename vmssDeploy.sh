1b253cd92d62e336553644629ae24f629a2aca10
Tam Huynh <tamhuynh@Tams-MacBook.local>
2019-01-15 13:29:14 -0800

    Add Az CLI

--- vmssDeploy.sh
+++ vmssDeploy.sh
@@ -1,11 +1,8 @@
-# Log activity
-LOG_PATH=/tmp/azuredeploy.log
-
 # Log parameters passed to this script. 
 echo $@ 
 
 # Basic info
-date > $LOG_PATH 2>&1
+date 
 whoami 
 
 # Store parameters passed to this script
@@ -14,16 +11,18 @@ NUM_OF_DATA_DISKS=${1}
 # Set the /cockroach-data data disk path
 COCKROACHDB_PATH=/cockroach-data
 
-# # install Azure CLI
-# sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y 
-# AZ_REPO=$(lsb_release -cs) 
-# echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-#     sudo tee /etc/apt/sources.list.d/azure-cli.list 
-# sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
-#      --keyserver packages.microsoft.com \
-#      --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF 
-# sudo apt-get update 
-# sudo apt-get install azure-cli 
+# install Azure CLI
+sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y 
+AZ_REPO=$(lsb_release -cs) 
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list 
+sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
+     --keyserver packages.microsoft.com \
+     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF 
+sudo apt-get update 
+sudo apt-get install azure-cli 
+
+az login --identity  
 
 mkdir $COCKROACHDB_PATH 
 if [ $NUM_OF_DATA_DISKS -eq 1 ]; then
