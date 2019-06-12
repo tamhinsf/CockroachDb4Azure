e7813d4dee9f5807ad91f96b7ac19096bf1f9330
tamhinsf <tamhinsf>
2019-06-11 23:18:59 -0700

    Fix install URL to use Linux

--- jumpBoxDeploy.sh
+++ jumpBoxDeploy.sh
@@ -13,8 +13,8 @@ mkdir $COCKROACHDB_PATH
 useradd cockroach 
 chown cockroach $COCKROACHDB_PATH 
 
-curl https://binaries.cockroachdb.com/cockroach-v19.1.1.darwin-10.9-amd64.tgz | tar -xJ
-cp -i cockroach-v19.1.1.darwin-10.9-amd64/cockroach /usr/local/bin
+wget -qO- https://binaries.cockroachdb.com/cockroach-v19.1.1.linux-amd64.tgz | tar  xvz
+cp -i cockroach-v19.1.1.linux-amd64/cockroach /usr/local/bin
 
 # sudo apt-get install nfs-kernel-server -y >> /tmp/azuredeploy.log.$$ 2>&1
 # echo "/$COCKROACHDB_PATH 10.0.0.0/16(rw)" | sudo tee -a /etc/exports >> /tmp/azuredeploy.log.$$ 2>&1
