277190bbcedd35c9f8776e8cf3745eed05f07d77
tamhinsf <tamhinsf>
2019-07-09 00:20:25 -0700

    standard lb, simplifying scripts

--- jumpBoxDeploy.sh
+++ jumpBoxDeploy.sh
@@ -4,6 +4,7 @@ az login --identity
 # get information about the current environment
 AZ_RG_NAME=`curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/resourceGroupName?api-version=2018-10-01&format=text"`
 LB_PIP_NAME=`az resource list --tag crdb=crdb-lb-pip --query [].name -o tsv`
+VMSS_NAME='az resource list --tag crdb=crdb-vmss --query [].name -o tsv'
 
 # get the resource name of the keyvault using tags
 KEYVAULT_NAME=`az resource list --tag crdb=crdb-keyvault --query [].name -o tsv`
@@ -16,7 +17,7 @@ AZ_LB_PUBLIC_HOSTNAME=`az network public-ip show --name $LB_PIP_NAME --resource-
 AZ_LB_PUBLIC_IP=`az network public-ip show --name $LB_PIP_NAME --resource-group $AZ_RG_NAME --query ipAddress | tr -d '"'`
 
 cockroach init --certs-dir=$COCKROACHDB_CERTS_PATH --host=$AZ_LB_PUBLIC_HOSTNAME
-cockroach sql  --certs=dir=$COCKROACHDB_CERTS_PATH --host=$AZ_LB_PUBLIC_HOSTNAME --execute="CREATE USER cradmin WITH PASSWORD 'cradmin'"
+cockroach sql  --certs-dir=$COCKROACHDB_CERTS_PATH --host=$AZ_LB_PUBLIC_HOSTNAME --execute="CREATE USER cradmin WITH PASSWORD 'cradmin'"
 
 # Exit script with 0 code to tell Azure that the deployment is done
 exit 0 
--- vmssDeploy.sh
+++ vmssDeploy.sh
@@ -1,15 +1,8 @@
 #!/bin/sh
 
 # install Azure CLI
-sudo apt-get install apt-transport-https lsb-release software-properties-common dirmngr -y 
-AZ_REPO=$(lsb_release -cs) 
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-    sudo tee /etc/apt/sources.list.d/azure-cli.list 
-sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
-     --keyserver packages.microsoft.com \
-     --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF 
 sudo apt-get update 
-sudo apt-get install azure-cli 
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash 
 
 # Create a cockroach user
 COCKROACH_USER=cockroach
